2714b41274c272d7e360adbd093444db7e21d201
dogofallstreets <22567725+marouane159@users.noreply.github.com>
2025-10-04 15:58:52 +0100

    Update Alerto.py

--- Alerto.py
+++ Alerto.py
@@ -86,7 +86,7 @@ STOCKS_DICT = {stock["symbol"]: stock for stock in BASE_STOCKS}
 # --- Authentication Setup ---
 ADMIN_USERNAME = "risk"
 ADMIN_PASSWORD = "@risk@"
-PUBLIC_PASSWORD = "masi19000"
+PUBLIC_PASSWORD = "genzmasi"
 
 def check_admin_credentials(username, password):
     """Check if the provided credentials match the admin credentials."""
